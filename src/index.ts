ae44129b90d6ae625e1ee1922b040d48fb3777d9
Gabriel Favot <gfavot@manager.one>
2022-08-06 13:17:25 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { Probot } from "probot";
+
+const typoList = ["niel", "nil", "nile"]; // TO COMPLET BY NEIL
+
+const waringMessage = "IT'S *NEIL*";
+
+function containsTypo(comment: string): boolean {
+  return typoList.some((typo) => comment.includes(typo));
+}
+
+export = (app: Probot) => {
+  app.on("pull_request_review_comment", async (context) => {
+    if (
+      context.payload.action === "created" &&
+      containsTypo(context.payload.comment.body)
+    ) {
+      context.octokit.pulls.createReplyForReviewComment({
+        body: waringMessage,
+        comment_id: context.payload.comment.id,
+        pull_number: context.payload.pull_request.id,
+        repo: context.payload.repository.url,
+        owner: "BOT",
+      });
+    }
+  });
+};
